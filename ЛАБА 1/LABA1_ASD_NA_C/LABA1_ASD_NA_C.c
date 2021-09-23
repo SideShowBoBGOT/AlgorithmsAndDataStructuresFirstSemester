ae4d92fadfac8163fdb2f9f3ec39ad264029d76c
Serhii Panchenko <67863015+SideShowBoBGOT@users.noreply.github.com>
2021-09-23 10:58:02 +0300

    1

---
+++ ЛАБА 1/LABA1_ASD_NA_C/LABA1_ASD_NA_C.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double number;
+    printf("Enter 3digit integer: ");
+    scanf_s("%lf", &number);
+    double hundreds = floor(number/100);
+    double dozens = floor(((number / 100 - hundreds) * 10));
+    double ones = (((number / 100 - hundreds) * 10) - dozens) * 10;
+    double average = (hundreds + dozens + ones) / 3;
+    printf("Dozens: %lf\n", dozens);
+    printf("Ones: %lf\n", ones);
+    printf("Average: %lf\n", average);
+    system("pause");
+    return 0;
+}
+
+
